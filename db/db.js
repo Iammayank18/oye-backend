b8d87a37797d78167452c87e2ce1a8a0363bd9b3
MAYANK KUMAR THAKUR <mayank.sanyank@gmail.com>
2022-09-16 14:31:21 +0530

    added public

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
-const DB = process.env.DBLOCAL;
+const DB = process.env.DBLIVE;
 
 mongoose
   .connect(`${DB}`, {
